6b2ab760d96982c88b4ebd7447f75efdd2014e27
Lookintothesoul <lookintothesoul@gmail.com>
2022-03-17 20:42:16 +0300

    11111

---
+++ src/pages/ExchangeRates/request/getExchangeRatesApi.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const getExchangeRatesApi = () => axios.get('https://www.cbr-xml-daily.ru/daily_json.js')
+
+export const getExchangeRatesOldApi = (prevUrl) => axios.get('https:' + prevUrl)
